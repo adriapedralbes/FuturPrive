5dba1545cc78faf5ee4d85f293fcdc79fea0afc9
adriapedralbes <a23adrestmar@inspedralbes.cat>
2025-04-04 21:35:07 +0200

    feat: Implementar página de Consultoría y componentes relacionados

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
+import { ToastProvider } from '@/components/ui/toast';
+
 import type { Metadata } from "next";
 import "./globals.css";
+import "../styles/transitions.css";
 
 export const metadata: Metadata = {
   title: "Adrià Estévez",
@@ -16,6 +19,7 @@ export default function RootLayout({
       <body className="!font-default overflow-x-hidden bg-background text-foreground antialiased">
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] bg-neutral-950">
           {children}
+          <ToastProvider />
         </div>
       </body>
     </html>
